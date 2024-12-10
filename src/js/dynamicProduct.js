370c2a222c6c2fc226e2db0fc8e20a1cfcb1cc47
Your Name <you@example.com>
2024-12-11 04:05:08 +0800

    product swatches added

---
+++ src/js/dynamicProduct.js
@@ -0,0 +1,40 @@
+const fetchData = async () => {
+  const response = await fetch("/data/data.json");
+  const data = await response.json();
+  return data;
+};
+
+const dynamicProduct = async () => {
+  const datas = await fetchData();
+
+  const imageContainer = document.querySelector(".swatches_image_container");
+
+  datas.forEach((data) => {
+    imageContainer.innerHTML += `<figure class="swatch_container" tabindex="0" role="button" aria-label="${data.color} color swatch">
+                <img src=${data.image} alt="" />
+              </figure>`;
+  });
+
+  dynamicChange(datas);
+};
+
+const dynamicChange = (datas) => {
+  const swatchContainer = document.querySelectorAll(".swatch_container");
+  const productImage = document.querySelector(".product_image");
+  const swatchName = document.querySelector(".swatch_color_name");
+  swatchName.textContent = "black";
+  swatchContainer.forEach((swatch, index) => {
+    swatch.addEventListener("click", () => {
+      const get = datas.find((data) => data.id === index + 1);
+
+      document.startViewTransition(() => {
+        productImage.src = `${get.image}`;
+        swatchName.textContent = ` ${get.color}`;
+      });
+    });
+  });
+};
+
+dynamicProduct();
+
+export default dynamicProduct;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import "/src/js/mobileMenuAnims.js";
 import "/src/js/navigationBgAnims.js";
 import "/src/js/smoothScrolling.js";
+import "/src/js/dynamicProduct.js";
